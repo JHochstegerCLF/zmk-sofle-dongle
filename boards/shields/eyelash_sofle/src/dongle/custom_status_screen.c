08f45aad70a0b4a36550336597205fcef579ab83
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2026-04-08 11:02:54 +0200

    fix: restore standard ext-power name and add display logging

--- boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
+++ boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
@@ -2,9 +2,14 @@
 #include <zmk/display/widgets/layer_status.h>
 #include <lvgl.h>
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
 static struct zmk_widget_layer_status layer_widget;
 
 lv_obj_t *zmk_display_status_screen() {
+    LOG_INF("Custom status screen initializing...");
+
     lv_obj_t *screen;
     screen = lv_obj_create(NULL); 
 
@@ -19,5 +24,6 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_style_text_color(zmk_widget_layer_status_obj(&layer_widget), lv_color_white(), 0);
     lv_obj_align(zmk_widget_layer_status_obj(&layer_widget), LV_ALIGN_CENTER, 0, 10);
 
+    LOG_INF("Custom status screen initialized.");
     return screen;
 }
