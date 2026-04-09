5cea348aa66568ff30e1cc5f9e933e09c9e8cb1c
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2026-04-10 01:43:06 +0200

    Removed Layer Label for just the Layer Status Widget in the center

--- boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
+++ boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
@@ -27,15 +27,15 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_align(zmk_widget_output_status_obj(&output_widget), LV_ALIGN_TOP_RIGHT, -5, 5);
 
     // 3. Layer Status Title
-    lv_obj_t *header = lv_label_create(screen);
-    lv_label_set_text(header, "LAYER:");
-    lv_obj_set_style_text_color(header, lv_color_white(), 0);
-    lv_obj_align(header, LV_ALIGN_CENTER, 0, -10);
+//    lv_obj_t *header = lv_label_create(screen);
+//    lv_label_set_text(header, "LAYER:");
+//    lv_obj_set_style_text_color(header, lv_color_white(), 0);
+//    lv_obj_align(header, LV_ALIGN_CENTER, 0, -10);
 
     // 4. Layer Status Widget (Center)
     zmk_widget_layer_status_init(&layer_widget, screen);
     lv_obj_set_style_text_color(zmk_widget_layer_status_obj(&layer_widget), lv_color_white(), 0);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_widget), LV_ALIGN_CENTER, 0, 10);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_widget), LV_ALIGN_CENTER, 0, 0);
 
     return screen;
 }
