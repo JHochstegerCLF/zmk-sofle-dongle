e5b692c2a62bfbfb9e8c5a95fdad369776ad6003
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2026-04-09 23:19:41 +0200

    debug: add BAT label to verify battery widget position

--- boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
+++ boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
@@ -14,9 +14,15 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_style_bg_color(screen, lv_color_black(), 0);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
 
-    // 1. Add the Battery Status widget (typically shows dongle battery)
+    // 1. Add the Battery Status widget
     zmk_widget_battery_status_init(&battery_widget, screen);
     lv_obj_align(zmk_widget_battery_status_obj(&battery_widget), LV_ALIGN_TOP_RIGHT, -5, 5);
+
+    lv_obj_t *bat_label = lv_label_create(screen);
+    lv_label_set_text(bat_label, "BAT");
+    lv_obj_set_style_text_color(bat_label, lv_color_white(), 0);
+    lv_obj_set_style_text_font(bat_label, &lv_font_montserrat_10, 0);
+    lv_obj_align_to(bat_label, zmk_widget_battery_status_obj(&battery_widget), LV_ALIGN_OUT_LEFT_MID, -2, 0);
 
     // 2. Add a static title for the layer
     lv_obj_t *header = lv_label_create(screen);
