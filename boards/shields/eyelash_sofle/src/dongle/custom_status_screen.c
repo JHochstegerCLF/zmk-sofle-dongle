c86414fd679e318c8cccc54e1c9dfbf4d290fbc0
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2026-04-09 23:29:16 +0200

    feat: use output_status widget to show USB/BT status on dongle

--- boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
+++ boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
@@ -1,10 +1,10 @@
 #include <zmk/display/status_screen.h>
 #include <zmk/display/widgets/layer_status.h>
-#include <zmk/display/widgets/battery_status.h>
+#include <zmk/display/widgets/output_status.h>
 #include <lvgl.h>
 
 static struct zmk_widget_layer_status layer_widget;
-static struct zmk_widget_battery_status battery_widget;
+static struct zmk_widget_output_status output_widget;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
@@ -14,15 +14,9 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_style_bg_color(screen, lv_color_black(), 0);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
 
-    // 1. Add the Battery Status widget
-    zmk_widget_battery_status_init(&battery_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_widget), LV_ALIGN_TOP_RIGHT, -5, 5);
-
-    lv_obj_t *bat_label = lv_label_create(screen);
-    lv_label_set_text(bat_label, "BAT");
-    lv_obj_set_style_text_color(bat_label, lv_color_white(), 0);
-    lv_obj_set_style_text_font(bat_label, &lv_font_montserrat_12, 0);
-    lv_obj_align_to(bat_label, zmk_widget_battery_status_obj(&battery_widget), LV_ALIGN_OUT_LEFT_MID, -2, 0);
+    // 1. Add the Output Status widget (shows USB/Bluetooth profile)
+    zmk_widget_output_status_init(&output_widget, screen);
+    lv_obj_align(zmk_widget_output_status_obj(&output_widget), LV_ALIGN_TOP_MID, 0, 5);
 
     // 2. Add a static title for the layer
     lv_obj_t *header = lv_label_create(screen);
