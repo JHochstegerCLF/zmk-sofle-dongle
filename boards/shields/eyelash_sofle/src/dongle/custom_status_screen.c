3955a0618d9283a1310e7f005ba60db6cc2bdaf2
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2026-04-10 01:35:33 +0200

    feat: re-add battery and output status widgets

--- boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
+++ boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
@@ -1,8 +1,12 @@
 #include <zmk/display/status_screen.h>
 #include <zmk/display/widgets/layer_status.h>
+#include <zmk/display/widgets/output_status.h>
+#include <zmk/display/widgets/battery_status.h>
 #include <lvgl.h>
 
 static struct zmk_widget_layer_status layer_widget;
+static struct zmk_widget_output_status output_widget;
+static struct zmk_widget_battery_status battery_widget;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
@@ -12,13 +16,23 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_style_bg_color(screen, lv_color_black(), 0);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
 
-    // 1. Add a simple title
+    // 1. Dongle Battery (Top Left)
+    zmk_widget_battery_status_init(&battery_widget, screen);
+    lv_obj_set_style_text_color(zmk_widget_battery_status_obj(&battery_widget), lv_color_white(), 0);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_widget), LV_ALIGN_TOP_LEFT, 5, 5);
+
+    // 2. Output Status (Top Right)
+    zmk_widget_output_status_init(&output_widget, screen);
+    lv_obj_set_style_text_color(zmk_widget_output_status_obj(&output_widget), lv_color_white(), 0);
+    lv_obj_align(zmk_widget_output_status_obj(&output_widget), LV_ALIGN_TOP_RIGHT, -5, 5);
+
+    // 3. Layer Status Title
     lv_obj_t *header = lv_label_create(screen);
     lv_label_set_text(header, "LAYER:");
     lv_obj_set_style_text_color(header, lv_color_white(), 0);
     lv_obj_align(header, LV_ALIGN_CENTER, 0, -10);
 
-    // 2. Add the dynamic Layer Status widget
+    // 4. Layer Status Widget (Center)
     zmk_widget_layer_status_init(&layer_widget, screen);
     lv_obj_set_style_text_color(zmk_widget_layer_status_obj(&layer_widget), lv_color_white(), 0);
     lv_obj_align(zmk_widget_layer_status_obj(&layer_widget), LV_ALIGN_CENTER, 0, 10);
