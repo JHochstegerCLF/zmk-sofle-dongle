2a76edd8252061e96386d7fa43d7b939cc0cc798
Jonas Hochsteger <jonas.hochsteger.04@gmail.com>
2026-04-09 23:10:21 +0200

    feat: add Peripheral Status widget for connection and battery monitoring

--- boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
+++ boards/shields/eyelash_sofle/src/dongle/custom_status_screen.c
@@ -1,27 +1,34 @@
 #include <zmk/display/status_screen.h>
 #include <zmk/display/widgets/layer_status.h>
+#include <zmk/display/widgets/peripheral_status.h>
 #include <lvgl.h>
 
 static struct zmk_widget_layer_status layer_widget;
+static struct zmk_widget_peripheral_status peripheral_widget;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
     screen = lv_obj_create(NULL); 
     
-    // Set background to black (all pixels off)
+    // Set background to black
     lv_obj_set_style_bg_color(screen, lv_color_black(), 0);
     lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
 
-    // 1. Add a simple title
+    // 1. Add the Peripheral Status widget at the top
+    // This shows if the left and right halves are connected and their battery
+    zmk_widget_peripheral_status_init(&peripheral_widget, screen);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_widget), LV_ALIGN_TOP_MID, 0, 5);
+
+    // 2. Add a static title for the layer
     lv_obj_t *header = lv_label_create(screen);
     lv_label_set_text(header, "LAYER:");
     lv_obj_set_style_text_color(header, lv_color_white(), 0);
-    lv_obj_align(header, LV_ALIGN_CENTER, 0, -10);
+    lv_obj_align(header, LV_ALIGN_CENTER, 0, 0);
 
-    // 2. Add the dynamic Layer Status widget
+    // 3. Add the Layer Status widget below the title
     zmk_widget_layer_status_init(&layer_widget, screen);
     lv_obj_set_style_text_color(zmk_widget_layer_status_obj(&layer_widget), lv_color_white(), 0);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_widget), LV_ALIGN_CENTER, 0, 10);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_widget), LV_ALIGN_CENTER, 0, 15);
 
     return screen;
 }
